a2eb318342264e8c2e7d06b3e0072138b92cc2f3
Jusair <jusairjsr123@gmail.com>
2025-04-22 21:05:10 +0530

    inshallah

--- intellicity/settings.py
+++ intellicity/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'intellicity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'intellicity_college_project',
+        'USER': 'postgres',
+        'PASSWORD': 'jusair',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
